2c064a35a32d8dcf3f81b5df62911c06390e21ad
kawewut <kawewut.chu@gmail.com>
2017-07-26 15:57:47 +0700

    fixed verify.php bot.php

--- verify.php
+++ verify.php
@@ -1,5 +1,6 @@
 <?php
-
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:tojPoVjG1wfe7JG';
 $access_token = 'tGB5WH9AfUCs6PIE3QKcGkrnnvqgNDCwep/5ZsEABIesFgYnZH6SH0r4XTl7L+mS7zyrnSzlisMTC7ZWMmUTdCEdsUwFBya1Hm5LJaHH4oZm6fVioynJpfytNmc1nb2EeLso66pBaRgK45y0/eqwBwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
@@ -10,6 +11,9 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+
 $result = curl_exec($ch);
 curl_close($ch);
 
