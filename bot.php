86ffea857ff4bfba545fb9b841e88f230679cbb3
kawewut <kawewut.chu@gmail.com>
2017-07-26 22:30:07 +0700

    fixed method get คำถาม

--- bot.php
+++ bot.php
@@ -72,6 +72,55 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
+
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == 'คำถาม') {
+			$replyToken = $event['replyToken'];
+			
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			
+			$data = [
+				'replyToken' => $replyToken,
+				'type' => 'template',
+  				'altText' => 'this is a confirm template',
+  				'template' => [
+					'type' => 'confirm',
+					'text' => 'Are you sure?',
+					'actions' => array(
+						[
+							'type' => 'message',
+							'label' => 'Yes',
+							'text' => 'yes'
+						],
+						[
+							'type' => 'message',
+							'label' => 'No',
+							'text' => 'no'
+						]
+      				
+					)
+				]
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+
 	}
 }
 echo "OK";
